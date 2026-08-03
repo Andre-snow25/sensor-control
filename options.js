bb40932885a88bfd6173592e330389cfb244a7ce
Andre Chiaramonti <achiaramonti4@gmail.com>
2026-08-03 14:02:24 -0300

    Add files via upload

--- options.js
+++ options.js
@@ -22,7 +22,7 @@ const ROSCA_OPTS = ['M8', 'M12', 'M18', 'M30'];
 const IP_OPTS = ['IP65', 'IP67', 'IP68', 'IP69K'];
 const CONEXAO_OPTS = ['Conector M8', 'Conector M12', 'Cabo integral 2m', 'Cabo integral 5m', 'Jumper M8xM12'];
 const MATERIAL_OPTS = ['Latão niquelado', 'Aço inox', 'PBT (plástico)', 'ABS'];
-const APLICACAO_OPTS = ['Detecção de metal', 'Detecção de nível', 'Detecção de presença', 'Contagem de peças', 'Posicionamento de cilindro', 'Conexão de sensores', 'Segurança', 'Detecção de Posição'];
+const APLICACAO_OPTS = ['Detecção de metal', 'Detecção de nível', 'Detecção de presença', 'Contagem de peças', 'Posicionamento de cilindro', 'Conexão de sensores', 'Segurança', 'Detecção de Posição', 'Motores elétricos'];
 const RECURSOS_OPTS = ['Ajuste de sensibilidade', 'LED indicador', 'Blindado', 'Não blindado', 'Resistente a EMI', 'Saída temporizada'];
 
 const CILINDRO_TIPO_OPTS = ['Magnético', 'Indutivo'];
@@ -49,7 +49,8 @@ const CAMPOS_POR_TIPO = {
   'Botoeira': ['tipoSaida', 'logica', 'tensao', 'formato', 'ip', 'conexao', 'material', 'aplicacao'],
   'Cabo de Sensor': ['formato', 'ip', 'conexao', 'material', 'aplicacao', 'genero', 'pinos', 'tamanho'],
   'Chave de Segurança': ['logica', 'tensao', 'formato', 'ip', 'material', 'aplicacao'],
-  'Chave Fim de Curso': ['logica', 'formato', 'ip', 'material', 'aplicacao']
+  'Chave Fim de Curso': ['logica', 'formato', 'ip', 'material', 'aplicacao'],
+  'Ponte Retificadora': ['aplicacao']
 };
 
 // Sobrescreve as opções do campo "Formato" pra tipos específicos.
